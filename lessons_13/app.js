9f0995edc0979628e60e0930f71361fd8f2cd1a7
charlesbchv <elbebatchaev@gmail.com>
2024-01-29 21:51:46 +0100

    [v-bind, :syntax] : like URL::to

---
+++ lessons_13/app.js
@@ -0,0 +1,36 @@
+const app = Vue.createApp({
+    //function, data
+    //template: '<h2 style="color:red">Je suis createApp du app.js</h2>'
+    data() {
+        return { //return object
+            url: 'https://www.youtube.com/watch?v=4deVCNJq3qc',
+            showBooks: true,
+            books: [
+                {title: 'name of the wind', author: 'patrick rothfuss', age: '40', img: 'assets/1.jpg'},
+                {title: 'i am legend', author: 'Francis Lawrence', age: '45', img: 'assets/2.jpg'},
+                {title: 'seigneur des anneaux', author: 'Peter Jackson', age: '32', img: 'assets/3.jpg'},
+                {title: 'At The Center of The Storm', author: 'George Tenet', age: '30', img: 'assets/4.jpg'},
+            ]
+        }
+    },
+    methods: { //methods object to store methods (functions) that we want to use in our app (in our template) 
+        toggleShowBooks() {
+            this.showBooks = !this.showBooks
+        },
+        handleEvent(e, data) {
+            console.log(e, e.type);
+            if(data) {
+                console.log(data);
+            }
+        },
+        handleEventMouseMove(e) {
+            this.x = e.offsetX;
+            this.y = e.offsetY;
+        },
+    }
+});
+
+
+app.mount('#app');
+
+// v-on directive is used to listen to events (like onclick, onmouseover, onkeydown, etc)
